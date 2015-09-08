dc6eab81a8c7398ab47f5e7253a7d14e073e75b1
Jason Ng <jsnng@users.noreply.github.com>
2015-09-08 22:14:29 +1000

    added fuzzing engine/test harness

---
+++ Fuzzer.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python
+
+import inspect, os, re, subprocess, sys 
+# cmd output for the ck() function
+def ec(a):
+	c = ''
+	print ' - - - - - - - - - - - - - - - - - - - - '	
+	for b in a:
+		if ck(b) == True:
+			print ' + %s: Found... ' % b
+			c = b
+		else:
+			print ' + %s: Not Found... ' % b
+	if not c:
+		fi = inspect.getframeinfo(inspect.currentframe())
+		print ' - - - - - - - - - - - - - - - - - - - - '	
+		print ' + sys.exit() ' + inspect.stack()[0][3] + ' ' + str(fi.lineno)
+		print ' - - - - - - - - - - - - - - - - - - - - '	
+		sys.exit()
+	print ' - - - - - - - - - - - - - - - - - - - - '	
+
+# check if subprocess exists...	
+def ck(a):
+	try:
+		d = open(os.devnull, 'w')
+		subprocess.Popen([a], stdout = d, stderr = d).communicate()
+		d.close()
+	except OSError as e:
+		# ENOENT
+		if e.errno == os.errno.ENOENT:
+			return False
+	return True	
+
+# fuzzing engine
+def fe(ms):
+	return 
+if __name__ == '__main__':
+	ps = []
+	ms = []
+	ec(ps)
+	ec(ms)
+	
+	# RUN MS - START LOOP HERE?
