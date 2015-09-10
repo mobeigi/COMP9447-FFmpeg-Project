2a5be2be28af353e8363922b43bef0e13d868c41
Mohammad Ghasembeigi <webmaster@mohammadg.com>
2015-09-10 20:16:40 +1000

    Add metadata packet processing

--- MutantHorse/FLVFormat.cpp
+++ MutantHorse/FLVFormat.cpp
@@ -3,7 +3,8 @@
 #include <vector>
 
 namespace std {
-  void FLVFormat::mutate(const string input, const string output) {
+  void FLVFormat::mutate(const string input, const string output)
+  {
     //Create IFReader
     IFReader ifreader;
     
@@ -13,18 +14,45 @@ namespace std {
       return;
     }
 
-  //Create output file stream
-  ofstream ofs(output, std::ofstream::binary);
+    //Create output file stream
+    ofstream ofs(output, std::ofstream::binary);
 
-  //Read FLV header
-  vector<unsigned char> flvheader(9);
+    //Read FLV header
+    vector<unsigned char> flvheader(9);
 
-  size_t read = ifreader.read(flvheader.size(), flvheader.data());
+    size_t readBytes = ifreader.read(flvheader.size(), flvheader.data());
 
-  //Write FLV header
-  ofs.write((char *)flvheader.data(), flvheader.size());
+    //Write FLV header
+    ofs.write((char *)flvheader.data(), flvheader.size());
+
+    //Read Metadata Packet Header (15 Bytes)
+    vector<unsigned char> metadataPacket(15);
+
+    readBytes = ifreader.read(metadataPacket.size(), metadataPacket.data());
+
+    //Write Metadata Packet Header
+    ofs.write((char *)metadataPacket.data(), metadataPacket.size());
+
+    //Get payload size
+    size_t payloadSize = 0;
+    for (int i = 5; i < 8; ++i) { //bytes at index 5-7 (3 bytes) contain payload size, big endian
+      payloadSize = (payloadSize << 8) + (metadataPacket[i] & 0xFF);
+    }
+
+    //Read metadata payload (payloadSize Bytes)
+    vector<unsigned char> metadataPayload(payloadSize);
+
+    readBytes = ifreader.read(metadataPayload.size(), metadataPayload.data());
+
+    //Write metadata payload
+    ofs.write((char *)metadataPayload.data(), metadataPayload.size());
+
+    //For the remainder of the flv file
+    //Look at each packet
+    //Then write header as is while mutating the payload (data)
 
   }
+
   void FLVFormat::generate() {
     //Todo
   }
