7f8ef6d693e24837b931a1feae8085845a7d2d7b
Jason Ng <jsnng@users.noreply.github.com>
2015-09-14 20:05:04 +1000

    . :poo:

--- Fuzzer.py
+++ Fuzzer.py
@@ -1,47 +1,24 @@
 #! /usr/bin/python
 
-import hashlib, inspect, os, re, select, subprocess, sys, time
-def ck(a):
-	try:
-		d = open(os.devnull, 'w')
-		subprocess.Popen([a], stdout = d, stderr = d).communicate()
-		d.close()
-	except OSError as e:
-		# ENOENT
-		if e.errno == os.errno.ENOENT:
-			return False
-	return True
+import hashlib, inspect, os, re, select, sys, time
 
-def fe(ms):
-	return
+from subprocess import Popen, PIPE
+
 if __name__ == '__main__':
-	mutator = './MutantHorse/MutantHorse'
-	tofuzz = 'ffmpeg'
-	inputfn = '"/Users/jsfn660/Dropbox/Security Workshop Project/Video files for Testing/test.flv"'
-	debug = 'lldb'
 
-	i = 0
+
 	while True:
-		crashed = 0
-		i += 1
 
 		# calls the mutator
-		outputfn =  'output.flv'
-		cmd = ' '.join([mutator, inputfn, outputfn])
-		os.system(cmd)
-
-		# opens the program to be fuzzed
-		cmd = [debug, tofuzz, '--', '-i' ,outputfn, 'pisspot.flv']
-		process = subprocess.Popen(cmd, stdin=subprocess.PIPE,  stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
-
-		# reads stdout + stderr from the fuzzed program
-		while True:
-				sys.stdout.write(process.stdout.readline())
-		crashed = process.poll()
-		print crashed
-		if(crashed != 0):
-			print 'crashed'
-		else:
-			print 'no crash detected'
-
+		cmd = "".split() # add mutator <input> <output>
+		mutator = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+		
+		# call the program to be fuzzed
+		cmd = "(echo r) | gdb --silent --return-child-result --args ffmpeg/ffmeg -i mutated.flv output.avi > /dev/null 2>&1; echo $?".split()
+		check_return_code = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+		
+		# print return code and display
+		return_code = check_return_code.poll()
+		print return_code
 		break
+			
