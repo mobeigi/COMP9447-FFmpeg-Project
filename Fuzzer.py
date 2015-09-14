bfbb36bd0991eb378774ac8544c0545841b457ba
Jason Ng <jsnng@users.noreply.github.com>
2015-09-14 20:26:26 +1000

    . :poop:

--- Fuzzer.py
+++ Fuzzer.py
@@ -3,22 +3,40 @@
 import hashlib, inspect, os, re, select, sys, time
 
 from subprocess import Popen, PIPE
+def ck(a):
+	try:
+		d = open(os.devnull, 'w')
+		subprocess.Popen([a], stdout = d, stderr = d).communicate()
+		d.close()
+	except OSError as e:
+		# ENOENT
+		if e.errno == os.errno.ENOENT:
+			return False
+	return True
 
+def fe(ms):
+	return
 if __name__ == '__main__':
 
 
 	while True:
 
 		# calls the mutator
-		cmd = "".split() # add mutator <input> <output>
+		cmd = "".split()
 		mutator = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
 		
+		
+		# call process to print out "r"
+		
+		cmd = ['(echo r)']
+		print_r = Popen(cmd, stdout=PIPE)
+		
 		# call the program to be fuzzed
-		cmd = "(echo r) | gdb --silent --return-child-result --args ffmpeg/ffmeg -i mutated.flv output.avi > /dev/null 2>&1; echo $?".split()
-		check_return_code = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+		cmd = "gdb --silent --return-child-result --args ffmpeg/ffmeg -i mutated.flv output.avi".split()
+		check_return_code = Popen(cmd, stdin=print_r.stdout, stdout=PIPE, stderr=PIPE)
 		
 		# print return code and display
 		return_code = check_return_code.poll()
 		print return_code
 		break
-			
+				
