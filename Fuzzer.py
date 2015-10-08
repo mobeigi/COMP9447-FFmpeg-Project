129f591884dd09c3c0a179c724b32e62a8cd2583
iancwwong <wong.iancw@gmail.com>
2015-10-08 23:09:53 +1100

    ALL PROBLEMS RESOLVED! NOW ABLE TO FUZZ
    - First working fuzzer

--- Fuzzer.py
+++ Fuzzer.py
@@ -1,12 +1,13 @@
 #! /usr/bin/python
 
 import ConfigParser
-import os
+import os, binascii
 import signal
 import subprocess
 import sys
 
 from shutil import copy2
+import time
 
 def check_process_exists(process_path, devnull):
 	try:
@@ -43,17 +44,22 @@ if __name__ == '__main__':
 	if not os.path.exists(input_file_path):
 		sys.exit(1)
 	mutated_file_path = config.get('General','input_folder_path') + 'mutated.flv'
-	output_file_path = config.get('General','input_folder_path') + 'output.avi'
+	output_file_path = config.get('General','output_folder_path') + 'output.avi'
 
 	# check if file logging folder exists
 	if not os.path.exists(config.get('General','output_folder_path')):
 		os.mkdir(config.get('General','output_folder_path'))
+	if not os.path.exists(config.get('General','results_folder_path')):
+		os.mkdir(config.get('General','results_folder_path'))
 
+	counter = 200
+	while (counter != 0):
 
-	while True:
+		if (os.path.exists(output_file_path)):		
+			print "File exists!! There is an issue, and You are AWESOME!"
 
 		# calls the mutator
-		cmd = [config.get('General','fuzzer_path'), input_file_path, mutated_file_path]
+		cmd = [config.get('General','fuzzer_path'), input_file_path, mutated_file_path, "mutate"]
 		mutator = subprocess.Popen(cmd, stdout=devnull, stderr=devnull)
 
 		# call process to print out "r"
@@ -62,7 +68,7 @@ if __name__ == '__main__':
 
 		# call the program to be fuzzed
 		cmd = "gdb --silent --return-child-result --args".split()
-		cmd = cmd + [config.get('General','program_path'), mutated_file_path, output_file_path]
+		cmd = cmd + [config.get('General','program_path'), "-i", mutated_file_path, output_file_path]
 		gdb_output = subprocess.Popen(cmd, stdin=print_r.stdout, stdout=devnull, stderr=devnull)
 
 		# wait for gdb to terminate
@@ -70,34 +76,32 @@ if __name__ == '__main__':
 
 		# get return signal
 		return_code = gdb_output.poll()
-		print return_code
-
-		# if return_code is normal, delete mutated and mutated output
-		files_to_remove = [input_file_path, output_file_path]
-		if return_code == 0 or return_code == 1:
-			for fn in files_to_remove:
-				if os.path.exists(fn):
-					os.remove(fn)
-
-			break;
 
 		# make mutated file our new seed
 		copy2(mutated_file_path, input_file_path)
 		os.remove(mutated_file_path)
 
-
-
 		# otherwise store and clean up for repeat
-		while True:
-			rand = time.strftime("%d%m%Y")
-			rand = '_'.join(rand, os.urandom(16))
-			if not os.path.exists(config.get('General','output_folder_path') + rand):
-				break
-		os.mkdir(config.get('General','output_folder_path') + rand)
-		file_to_remove.append(mu)
-		for fn in file_to_remove:
-			if os.path.exists(fn):
-				copy2(fn, "/".join(config.get('General','output_folder_path'), rand, os.basename(fn)))
-				os.remove(fn)
-		break
+		# 0: successful execution
+		# 1: file exists, or empty file outputted
+		# 69: conversion failure
+		if (return_code != 0 and return_code != 1 and return_code != 69):
+
+			files_to_copy = [input_file_path, output_file_path]
+			while True:
+				rand = time.strftime("%d%m%Y")
+				rand += '_'  + binascii.b2a_hex(os.urandom(16)) + "_" + str(return_code)
+				if not os.path.exists(config.get('General','results_folder_path') + rand):
+					break
+			os.mkdir(config.get('General','results_folder_path') + rand)
+			files_to_copy.append(mutated_file_path)
+			for fn in files_to_copy:
+				if os.path.exists(fn):
+					tempstr = config.get('General', 'results_folder_path') + rand + "/" + os.path.basename(fn)
+					copy2(fn, tempstr)
+
+		if (os.path.exists(output_file_path)):		
+			os.remove(output_file_path)
+			
+		counter -= 1
 	print "Exiting.."
